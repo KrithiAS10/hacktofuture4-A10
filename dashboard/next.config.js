348e718be2cf61e35aaae438e2bfeb3cbe5d7ef2
KrithiAS10 <krithisonu.a.s@gmail.com>
2026-04-16 00:11:12 +0530

    fixes

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://lerna-backend:8000/api/:path*',
+      },
+    ]
+  },
 }
 module.exports = nextConfig
